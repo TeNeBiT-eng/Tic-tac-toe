22c127abeefdf779ad1ea0cb486a235e1bcc48fe
TeNeBiT-eng <ademolababalola2003@gmail.com>
2022-07-07 23:27:39 +0100

    script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 window.addEventListener("DOMContentLoaded", () => {
   const tiles = Array.from(document.querySelectorAll("tile"));
-  const playerDisplay = document.querySelector(".display_player");
+  const playerDisplay = document.querySelector(".display-player");
   const resetButton = document.querySelector("#reset");
   const announcer = document.querySelector(".announcer");
 
-  let board = ["", "", "", "", "", "", "", "", ""];
+  let board = ['', '', '', '', '', '', '', '', ''];
   let currentPlayer = "X";
   let isGameActive = true;
 
   const PLAYERX_WON = "PLAYERX_WON";
-  const PLAYERY_WON = "PLAYERY_WON";
+  const PLAYERO_WON = "PLAYERO_WON";
   const TIE = "TIE";
 
   /*    
@@ -20,15 +20,20 @@ window.addEventListener("DOMContentLoaded", () => {
     */
 
   const winningConditions = [
-    [0, 1, 2][(3, 4, 5)][(6, 7, 8)][(0, 3, 6)][(1, 4, 7)][(2, 5, 8)][(0, 4, 8)][
-      (2, 4, 6)
-    ],
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
   ];
 
   function handleResultValidation() {
     let roundWon = false;
     for (let i = 0; i <= 7; i++) {
-      const winCondition = winningCondition[i];
+      const winCondition = winningConditions[i];
       const a = board[winCondition[0]];
       const b = board[winCondition[1]];
       const c = board[winCondition[2]];
@@ -56,10 +61,10 @@ window.addEventListener("DOMContentLoaded", () => {
   const announce = (type) => {
     switch (type) {
       case PLAYERO_WON:
-        announcer.innerHTML = `Player <span class='playerO'>O</span> Won`;
+        announcer.innerHTML = `Player <span class='PlayerO'>O</span> Won`;
         break;
       case PLAYERX_WON:
-        announcer.innerHTML = `Player <span class='playerX'>X</span> Won`;
+        announcer.innerHTML = `Player <span class='PlayerX'>X</span> Won`;
         break;
       case TIE:
         announcer.innerText = "It's A Tie";
@@ -97,7 +102,7 @@ window.addEventListener("DOMContentLoaded", () => {
   };
 
   const resetBoard = () => {
-    board = ["", "", "", "", "", "", "", "", ""];
+    board = ['', '', '', '', '', '', '', '', ''];
     isGameActive = true;
     announcer.classList.add('hide');
 
